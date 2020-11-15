b65fa5a5784ff88be7ef6a84e0bf0bd9abe36d15
Patrick A <akolo@hotmail.com>
2020-11-15 16:43:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function writeCards(names, eventname){
   for (let i = 0; i < names.length; i++){
   newArray.push(console.log(`Thank you ${names[i]}, for the wonderful ${eventname} gift!`))
   }
-return(countDown);
+return newArray;
 }
 writeCards(names, eventname);
 
